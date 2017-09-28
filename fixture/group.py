92d495266366640216ba8f6318564917ae299bbf
Konstantin Ivanov <seyros@mail.ru>
2017-09-28 16:44:03 +0300

    ролик 4_5

--- fixture/group.py
+++ fixture/group.py
@@ -44,9 +44,12 @@ class GroupHelper:
         wd.find_element_by_link_text("group page").click()
 
     def delete_first_group(self):
+        wd.delete_group_by_index(0)
+
+    def delete_group_by_index(self, index):
         wd = self.app.wd
         self.open_groups_page()
-        self.select_first_group()
+        self.select_group_by_index(index)
         # submit deletion
         wd.find_element_by_name("delete").click()
         self.return_to_groups_page()
@@ -56,10 +59,14 @@ class GroupHelper:
         wd = self.app.wd
         wd.find_element_by_name("selected[]").click()
 
-    def modify_first_group(self, new_group_data):
+    def select_group_by_index(self, index):
+        wd = self.app.wd
+        wd.find_elements_by_name("selected[]")[index].click()
+
+    def modify_group_by_index(self, index, new_group_data):
         wd = self.app.wd
         self.open_groups_page()
-        self.select_first_group()
+        self.select_group_by_index(index)
         # open modification form
         wd.find_element_by_name("edit").click()
         # fill group form
@@ -68,6 +75,12 @@ class GroupHelper:
         wd.find_element_by_name("update").click()
         self.return_to_groups_page()
         self.group_cache = None
+
+
+    def modify_first_group(self, new_group_data):
+        self.modify_group_by_index(0)
+
+
 
     def count(self):
         wd = self.app.wd
@@ -86,4 +99,5 @@ class GroupHelper:
                 id = element.find_element_by_name("selected[]").get_attribute("value")
                 self.group_cache.append(Group(name = text, id = id))
         return list(self.group_cache)
+
 
--- test/test_del_group.py
+++ test/test_del_group.py
@@ -1,13 +1,15 @@
 # -*- coding: utf-8 -*-
 __author__ = 'ivanov'
 from model.group import Group
+from random import randrange
 
-def test_delete_first_group(app):
+def test_delete_some_group(app):
     if app.group.count() == 0:
         app.group.create(Group(name="test"))
     old_groups = app.group.get_group_list()
-    app.group.delete_first_group()
+    index = randrange(len(old_groups))
+    app.group.delete_group_by_index(index)
     new_groups = app.group.get_group_list()
     assert len(old_groups) - 1 == len(new_groups)  # проверка, что новый список групп на 1 элемент короче
-    old_groups[0:1] = []  # удаляем первый элемент из старого списка групп
-    assert old_groups == new_groups # проверяем, что новый список групп равен старому списку, из которого удалили первый элемент
+    old_groups[index:index+1] = []  # удаляем случайный элемент из старого списка групп
+    assert old_groups == new_groups # проверяем, что новый список групп равен старому списку, из которого удалили один элемент
