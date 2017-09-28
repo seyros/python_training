cbe4f33dee39b504e4871dc9f99fb9b753219406
Konstantin Ivanov <seyros@mail.ru>
2017-09-28 11:55:21 +0300

    ролик 4_2

--- test/test_del_group.py
+++ test/test_del_group.py
@@ -8,4 +8,6 @@ def test_delete_first_group(app):
     old_groups = app.group.get_group_list()
     app.group.delete_first_group()
     new_groups = app.group.get_group_list()
-    assert len(old_groups) - 1 == len(new_groups)
+    assert len(old_groups) - 1 == len(new_groups)  # проверка, что новый список групп на 1 элемент короче
+    old_groups[0:1] = []  # удаляем первый элемент из старого списка групп
+    assert old_groups == new_groups # проверяем, что новый список групп равен старому списку, из которого удалили первый элемент
