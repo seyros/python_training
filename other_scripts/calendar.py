06983185cc0002cddbf6bc6790acfdd0d7703ac1
seyros <seyros@mail.ru>
2018-08-06 22:50:10 +0300

    Create calendar.py

---
+++ other_scripts/calendar.py
@@ -0,0 +1,12 @@
+import date
+
+Class Calendar:
+  #   return day e.g. 31
+  def get_day():
+    pass
+  #   return month e.g. 3
+  def get_month():
+    pass
+  #   return year e.g. 1988
+  def get_year():
+pass
